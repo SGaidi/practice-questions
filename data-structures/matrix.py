c96bc9ba7e0bab18a600cbfc9a6165c61e85aac8
Sarah Gaidi <gaidi.sarah@gmail.com>
2021-03-28 21:09:36 +0300

    added usage of numpy in matrix.py

--- data-structures/matrix.py
+++ data-structures/matrix.py
@@ -79,3 +79,51 @@ def test_spiralShift() -> None:
 	spiralShift(matrix)
 	for row in matrix:
 		print(row)
+
+
+# Question 4
+
+
+def random_matrix(size: int) -> np.array:
+    return np.random.randint(0, size, size=(size, size))
+
+
+def mat_djikstra():
+    map_size = 10
+    my_map = random_matrix(map_size)
+    source_indices = (0, 0)
+    target_indices = (map_size-1, map_size-1)
+    
+    indices_to_length = {}
+    unseen_indices = [source_indices]  # queue
+    
+    while unseen_indices:
+        current_indices = unseen_indices.pop(0)
+        row, col = current_indices
+        steps = my_map[row][col]
+        next_indices = (
+            (row + steps, col),
+            (row, col + steps),
+            (row - steps, col),
+            (row, col - steps),
+        )
+        
+        for next_row, next_col in next_indices:
+            if next_row < 0 or next_row >= len(my_map):
+                continue
+            if next_col < 0 or next_col >= len(my_map[0]):
+                continue
+            
+            current_length = indices_to_length.get(current_indices, 0)
+            best_next_length = indices_to_length.get((next_row, next_col), float('inf'))
+            
+            if best_next_length == float('inf') or best_next_length > current_length + steps:
+                indices_to_length[(next_row, next_col)] = current_length + steps
+                unseen_indices.append((next_row, next_col))
+    
+    if target_indices not in indices_to_length:
+        raise ValueError("not found")
+    
+    return indices_to_length[target_indices]
+
+
