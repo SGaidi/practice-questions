6fc8b3b4fc4ecca76893eb006803618406340535
Sarah Gaidi <gaidi.sarah@gmail.com>
2021-04-08 13:03:00 +0300

    modify status.py script

--- status.py
+++ status.py
@@ -1,34 +1,36 @@
-import typing
 from typing import List, Tuple
 
 
-def read_db() -> List[Tuple]:
-	data = []
+def read_csv() -> List[Tuple]:
+	rows = []
 	with open('practice_questions.csv', 'r') as f:
 		lines = f.readlines()
 	for line in lines:
 		name, done, total = line[:-1].split(',')
 		done, total = int(done), int(total)
-		precent = round(done / total, 3)
-		data.append((name, done, total, precent))
-	return data
+		percent = round(done / total, 3)
+		rows.append((name, done, total, percent))
+	return rows
 	
 
-def sorted_precent(db: List[Tuple]) -> List[Tuple]:
-	return sorted(db, key=lambda record: record[3])
+def sorted_percent(rows: List[Tuple]) -> List[Tuple]:
+	return sorted(rows, key=lambda record: record[3])
 	
 	
-def rate_of(db: List[Tuple], days_left: int):
-	return sum(record[2]-record[1] for record in db) / days_left
+def rate_of(rows: List[Tuple], days_left: int):
+	return sum(record[2]-record[1] for record in rows) / days_left
 	
 	
-def precent_of(db: List[Tuple]) -> int:
-	return 100 * sum(record[1] for record in db) / sum(record[2] for record in db)
+def percent_of(rows: List[Tuple]) -> int:
+	return 100 * sum(
+		record[1] for record in rows) / sum(record[2] for record in rows)
 	
 	
-db = read_db()
-rate = rate_of(db, 60)
-precent = precent_of(db)
-print('\n'.join(str(item) for item in sorted_precent(db)))
+data = read_csv()
+rate = rate_of(data, 60)
+percent = percent_of(data)
+print('Category,done,total,percent')
+print('%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+print('\n'.join(str(item) for item in sorted_percent(data)))
 print(f'daily rate to finish all questions in 2 months: {rate:.2f}')
-print(f'{precent:.2f}% of questions completed so far.')
+print(f'{percent:.2f}% of questions completed so far.')
